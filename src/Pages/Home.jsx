b5efd4882823581b0b3567896ebc00668fda4777
rashimarasangi <rashimarasangi200@gmail.com>
2025-08-09 15:42:06 +0530

    hfeatured section is added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../Components/Hero'
+import FeaturedSection from '../Components/FeaturedSection'
 
 const Home = () => {
   return (
     <>
       <Hero/>
+      <FeaturedSection/>
     </>
   )
 }
