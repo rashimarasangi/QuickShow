d93b631359ffba7f14371cb00d6aeebb110e26c7
rashimarasangi <rashimarasangi200@gmail.com>
2025-08-09 11:05:51 +0530

    projec is added

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+import Home from './Pages/Home'
+import Movies from './Pages/Movies'
+import MovieDetails from './Pages/MovieDetails'
+import SeatLayout from './Pages/SeatLayout'
+import MyBookings from './Pages/MyBookings'
+import Favourite from './Pages/Favourite'
+import {Toaster } from 'react-hot-toast'
+import Footer from './Components/Footer'
+
+const App = () => {
+  const location = useLocation();
+  const isAdminRoute = location.pathname.startsWith('/admin');
+
+  return (
+    <>
+    <Toaster/>
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id/:date' element={<SeatLayout />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
+        <Route path='/favourite' element={<Favourite />} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
+  );
+}
+
+export default App
