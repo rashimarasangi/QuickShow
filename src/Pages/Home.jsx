fb0bb5a98a60945c16ad211a5d1a2729b89c17b4
rashimarasangi <rashimarasangi200@gmail.com>
2025-08-10 13:34:21 +0530

    trailer section is added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero'
 import FeaturedSection from '../Components/FeaturedSection'
+import TrailerSection from '../Components/TrailerSection'
 
 const Home = () => {
   return (
     <>
       <Hero/>
       <FeaturedSection/>
+      <TrailerSection/>
     </>
   )
 }
