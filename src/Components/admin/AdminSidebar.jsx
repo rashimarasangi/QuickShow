7dd49ebc757ec2f8863fdc30fdd581ffc09644a4
rashimarasangi <rashimarasangi200@gmail.com>
2025-08-14 20:10:57 +0530

    admin sidebar  is added

--- src/Components/admin/AdminSidebar.jsx
+++ src/Components/admin/AdminSidebar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
 import { LayoutDashboardIcon, ListCollapseIcon, ListIcon, PlusSquareIcon } from 'lucide-react'
+import { use } from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 const AdminSidebar = () => {
 
@@ -19,7 +21,21 @@ const AdminSidebar = () => {
 
   return (
     <div className='h-[calc(100vh-64px)] md:flex flex-col items-center pt-8 max-w-13 md:max-w-60 w-full border-r border-gray-300/20 text-sm'>
-      
+      <img className='h-9 md:h-14 w-9 md:w-14 rounded-full mx-auto' src={user.imageUrl} alt="sidebar" />
+      <p className='mt-2 text-base max-md:hidden'>{user.firstName} {user.lastName}</p>
+      <div className='w-full'>
+        {adminNavLinks.map((link,index)=>(
+           <NavLink className={({isActive}) =>`relative flex items-center max-md:justify-center gap-2 w-full py-2.5 min-md:pl-10 first:mt-6 text-gray-400 ${isActive && 'bg-primary/15 text-primary group'}`}>
+            {({isActive})=>(
+              <>
+               <Link.icon className="w-5 h-5"/>
+               <p className='max-md:hidden'>{link.name}</p>
+               </>
+            )}
+
+           </NavLink>
+        ))}
+      </div>
     </div>
   )
 }
