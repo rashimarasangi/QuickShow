906e9c3dd58eaebcd2e04b2a10a56529bc56967c
rashimarasangi <rashimarasangi200@gmail.com>
2025-08-14 20:10:15 +0530

    admin sidebar  is added

--- src/Components/admin/AdminSidebar.jsx
+++ src/Components/admin/AdminSidebar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
+import { LayoutDashboardIcon, ListCollapseIcon, ListIcon, PlusSquareIcon } from 'lucide-react'
 
 const AdminSidebar = () => {
+
+  const user = {
+  firstName:'Admin',
+  lastName : 'User',
+  imageUrl: assets.profile,
+   }
+
+   const adminNavLinks = [
+    {name:'Dashboard', path:'/admin',icon:LayoutDashboardIcon},
+    {name:'Add Shows', path:'/admin/add-shows',icon:PlusSquareIcon},
+    {name:'List Shows', path:'/admin/lisi-shows',icon:ListIcon},
+    {name:'List Bookings', path:'/admin/list-bookings',icon:ListCollapseIcon},
+   ]
+
   return (
-    <div>
-      <h1>sidebar</h1>
+    <div className='h-[calc(100vh-64px)] md:flex flex-col items-center pt-8 max-w-13 md:max-w-60 w-full border-r border-gray-300/20 text-sm'>
+      
     </div>
   )
 }
