61f18adc03713c2b1b606329790383571d144515
rashimarasangi <rashimarasangi200@gmail.com>
2025-08-15 13:24:29 +0530

    list show section is  added

--- src/Pages/admin/ListShows.jsx
+++ src/Pages/admin/ListShows.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { dummyShowsData } from '../../assets/assets';
+import Loading from '../../Components/Loading';
+import Title from '../../Components/admin/Title';
 
 const ListShows = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+  const currency = import.meta.env.VITE_CURRENCY 
+
+  const [shows,setShows] = useState([]);
+  const [loading,setLoading] = useState(true);
+
+  const getAllShows = async () =>{
+    try {
+      setShows ([{
+        movie: dummyShowsData[0],
+        showDateTime: "2025-06-30T02:30:00.000Z",
+        showPrice:59,
+        occupiedSeats:{
+          A1:"user_1",
+          B1:"user_2",
+          C1:"user_3"
+        }
+
+      }]);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(()=> {
+    getAllShows();
+  },[])
+
+
+  return !loading ? (
+    <>
+     <Title text1='List' text2='Shows' />
+    </>
+  ) : <Loading/>
 }
 
 export default ListShows
