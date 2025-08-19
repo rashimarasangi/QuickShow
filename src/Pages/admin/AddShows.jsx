8dc0ff536b9535323d934c1f5bc35afe3435eb32
rashimarasangi <rashimarasangi200@gmail.com>
2025-08-19 14:08:17 +0530

    add shows section is  added

--- src/Pages/admin/AddShows.jsx
+++ src/Pages/admin/AddShows.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { dummyShowsData } from '../../assets/assets';
+
 
 const AddShows = () => {
+
+  const curency = import.meta.env.VITE_CURRENCY 
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+  const [selectedMovie,setSelectedMovie] = useState(null);
+  const [dateTimeSelection,setDateTimeSelection] =useState({});
+  const [dateTimeInput,setDateTimeInput] = useState("");
+  const [showprice,setShowPrice] = useState("");
+
+
+  const FetchNowPlayingMovies = async () => {
+    setNowPlayingMovies(dummyShowsData)
+  };
+
   return (
     <div>
       
