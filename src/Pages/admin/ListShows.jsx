dceb378c5d5a09a284479ef22372ab10cb76f1f7
rashimarasangi <rashimarasangi200@gmail.com>
2025-08-19 11:55:37 +0530

    updated list shows section is  added

--- src/Pages/admin/ListShows.jsx
+++ src/Pages/admin/ListShows.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { dummyShowsData } from '../../assets/assets';
 import Loading from '../../Components/Loading';
 import Title from '../../Components/admin/Title';
+import { dateFormat } from '../../lib/dateFormat';
 
 const ListShows = () => {
 
@@ -37,7 +38,29 @@ const ListShows = () => {
 
   return !loading ? (
     <>
-     <Title text1='List' text2='Shows' />
+    <Title text1="List" text2="Shows"/> 
+    <div className='max-w-4xl mt-6 overflow-x-auto'>
+      <table className='w-full border-collapse rounded-md overflow-hidden text-nowrap'>
+          <thead>
+            <tr className='bg-primary/20 text-left text-white'>
+              <th className='p-2 font-medium pl-5'>Movie Name</th>
+              <th className='p-2 font-medium'>Show Time</th>
+              <th className='p-2 font-medium'>Total Bookings</th>
+              <th className='p-2 font-medium'>Earnings</th>
+            </tr>
+          </thead>
+          <tbody className='text-sm font-light'>
+                {shows.map((show, index)=>(
+                  <tr key={index} className='border-b border-primary/10 bg-primary/5 even:bg-primary/10'>
+                    <td className='p-2 min-w-45 pl-5'>{show.movie.title}</td>
+                    <td className='p-2'>{dateFormat(show.showDateTime)}</td>
+                    <td className='p-2'>{Object.keys(show.occupiedSeats).length}</td>
+                    <td className='p-2'>{currency} {Object.keys(show.occupiedSeats).length * show.showPrice}</td>
+                  </tr>
+                ))}
+          </tbody>
+      </table>
+    </div>
     </>
   ) : <Loading/>
 }
