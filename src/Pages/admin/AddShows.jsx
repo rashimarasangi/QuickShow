0f5245f900ae781bc7ea93f0b48ef2552edcf116
rashimarasangi <rashimarasangi200@gmail.com>
2025-08-19 14:08:40 +0530

    add shows section is  added

--- src/Pages/admin/AddShows.jsx
+++ src/Pages/admin/AddShows.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import { dummyShowsData } from '../../assets/assets';
+import { useEffect } from 'react';
+import Loading from '../../Components/Loading';
+import Title from '../../Components/admin/Title';
 
 
 const AddShows = () => {
@@ -16,11 +19,27 @@ const AddShows = () => {
     setNowPlayingMovies(dummyShowsData)
   };
 
-  return (
-    <div>
-      
-    </div>
-  )
+  useEffect(()=>{
+    FetchNowPlayingMovies();
+  },[])
+
+  return nowPlayingMovies.length > 0 ? (
+    <>
+     <Title text1="Add" text2="Shows"/> 
+     <p className='mt-10 text-lg font-medium'>Now Playing Movies</p>
+     <div className='overflow-x-auto pb-4'>
+      <div className='group flex flex-wrap gap-4 mt-4 w-max'>
+         {nowPlayingMovies.map((movie)=>(
+          <div key ={movie.id} className={`relative max-w-40 cursor-pointer group-hover:not-hover:opacity-40 hover:translate-y-1 transition duration-300`} >
+            <div>
+              <img src={movie.poster_path} alt="" />
+            </div>
+            </div>
+         ))}
+      </div>
+     </div>
+    </>
+  ) : <Loading/>
 }
 
 export default AddShows
