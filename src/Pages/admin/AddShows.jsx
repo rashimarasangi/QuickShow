c87cf2e6c74e7d3db1e63164eec5039e78b8400b
rashimarasangi <rashimarasangi200@gmail.com>
2025-08-20 20:48:37 +0530

    updated add shows section is  added

--- src/Pages/admin/AddShows.jsx
+++ src/Pages/admin/AddShows.jsx
@@ -21,6 +21,22 @@ const AddShows = () => {
     setNowPlayingMovies(dummyShowsData)
   };
 
+  const handleDateTimeAdd = () => {
+    if(!dateTimeInput) return;
+    const [date,time] = dateTimeInput.split("T");
+    if (!date || !time) return;
+
+    setDateTimeSelection((prev)=>{
+      const times = prev[date] || [];
+      if(!times.includes(time)) {
+        return {...prev, [date]: [...times, time]};
+      }
+      return prev;
+    });
+  };
+
+  
+
   useEffect(()=>{
     FetchNowPlayingMovies();
   },[])
@@ -62,6 +78,15 @@ const AddShows = () => {
   </div>
 </div>
 
+{/* date and time */}
+
+<div className='mt-6'>
+  <label className='block text-sm font-medium mb-2'>Select Date and Time</label>
+  <div className='inline-flex gap-5 border border-gray-600 p-1 pl-3 rounded-lg'>
+    <input type="datetime-local" value={dateTimeInput} onChange={(e)=>setDateTimeInput(e.target.value)} className='outline-none rounded-md' />
+    <button onClick={handleDateTimeAdd} className='bg-primary/80 text-white px-3 py-2 text-sm rounded-lg hover:bg-primary cursor-pointer'>Add Time</button>
+  </div>
+</div>
     </>
   ) : <Loading/>
 }
