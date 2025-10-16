ea77152875f65d46e50c2485607f884c4551b0a8
rashimarasangi <rashimarasangi200@gmail.com>
2025-10-16 10:01:06 +0530

    updated appjx,featured section and hero section are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,46 @@
-import React from 'react'
-import { Route, Routes, useLocation } from 'react-router-dom'
-import Navbar from './Components/Navbar'
-import Home from './Pages/Home'
-import Movies from './Pages/Movies'
-import MovieDetails from './Pages/MovieDetails'
-import SeatLayout from './Pages/SeatLayout'
-import MyBookings from './Pages/MyBookings'
-import Favourite from './Pages/Favourite'
-import {Toaster } from 'react-hot-toast'
-import Footer from './Components/Footer'
-import Layout from './Pages/admin/Layout'
-import Dashboard from './Pages/admin/Dashboard'
-import AddShows from './Pages/admin/AddShows'
-import ListShows from './Pages/admin/ListShows'
-import ListBookings from './Pages/admin/ListBookings'
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import Movies from "./Pages/Movies";
+import MovieDetails from "./Pages/MovieDetails";
+import SeatLayout from "./Pages/SeatLayout";
+import MyBookings from "./Pages/MyBookings";
+import Favourite from "./Pages/Favourite";
+import { Toaster } from "react-hot-toast";
+import Footer from "./Components/Footer";
+import Layout from "./Pages/admin/Layout";
+import Dashboard from "./Pages/admin/Dashboard";
+import AddShows from "./Pages/admin/AddShows";
+import ListShows from "./Pages/admin/ListShows";
+import ListBookings from "./Pages/admin/ListBookings";
+import { FavouriteProvider } from "./contexts/FavouriteContext";
 
 const App = () => {
   const location = useLocation();
-  const isAdminRoute = location.pathname.startsWith('/admin');
+  const isAdminRoute = location.pathname.startsWith("/admin");
 
   return (
-    <>
-    <Toaster/>
+    <FavouriteProvider>
+      <Toaster />
       {!isAdminRoute && <Navbar />}
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/movies' element={<Movies />} />
-        <Route path='/movies/:id' element={<MovieDetails />} />
-        <Route path='/movies/:id/:date' element={<SeatLayout />} />
-        <Route path='/my-bookings' element={<MyBookings />} />
-        <Route path='/favourite' element={<Favourite />} />
-        <Route path='/admin/*' element={<Layout/>}>
-          <Route index element={<Dashboard/>}/>
-          <Route path="add-shows" element={<AddShows/>}/>
-          <Route path="list-shows" element={<ListShows/>}/>
-          <Route path="list-bookings" element={<ListBookings/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/favourite" element={<Favourite />} />
+        <Route path="/admin/*" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="add-shows" element={<AddShows />} />
+          <Route path="list-shows" element={<ListShows />} />
+          <Route path="list-bookings" element={<ListBookings />} />
         </Route>
       </Routes>
       {!isAdminRoute && <Footer />}
-    </>
+    </FavouriteProvider>
   );
-}
+};
 
-export default App
+export default App;
